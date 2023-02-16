4348663b6cac129d8ffff43265935e9116cae94c
Patryk.Ksiazek <patrykksiazek92@gmail.com>
2023-02-16 16:56:01 +0000

    js update

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 
-    const welcome = () => {
+const welcome = () => {
     console.log("Hello!");
 }
 
 const toggleBackground = () => {
- const body = document.querySelector(".body");
- const themeName = document.querySelector(".backgroundButton__themeName")
+    const body = document.querySelector(".body");
+    const themeName = document.querySelector(".backgroundButton__themeName")
 
- body.classList.toggle("darkBody");
- themeName.innerText = body.classList.contains("darkBody") ? "jasny" : "ciemny"
+    body.classList.toggle("darkBody");
+    themeName.innerText = body.classList.contains("darkBody") ? "jasny" : "ciemny"
 
 };
- 
+
 const init = () => {
     const backgroundButton = document.querySelector(".backgroundButton");
-    backgroundButton.addEventListener("click",toggleBackground)
+    backgroundButton.addEventListener("click", toggleBackground)
 
 
 
